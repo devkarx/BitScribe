f8b9e44e8889f91e3db9843e1c7b199ae85a91ea
devkarx <aradhydeokar@gmail.com>
2025-07-04 17:35:46 +0530

    Changed UI for button

--- src/components/Header/LogoutBtn.jsx
+++ src/components/Header/LogoutBtn.jsx
@@ -4,17 +4,22 @@ import { logout } from '../../store/authSlice'
 import authService from '../../appwrite/auth'
 
 function LogoutBtn() {
-    const dispatch=useDispatch()
-    const logoutHandler=()=>{
-        authService.logout().then(()=>{
+    const dispatch = useDispatch()
+    const logoutHandler = () => {
+        authService.logout().then(() => {
             dispatch(logout())
         })
     }
-  return (
-    <div>
-      <button onClick={logoutHandler}>Logout</button>
-    </div>
-  )
+    return (
+        <div>
+            <button 
+                onClick={logoutHandler}
+                className="px-4 py-2 bg-red-600 text-white rounded-lg hover:bg-red-700 transition-colors duration-200"
+            >
+                Logout
+            </button>
+        </div>
+    )
 }
 
 export default LogoutBtn
