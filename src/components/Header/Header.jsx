cefb256db83569714d73700dfa30318ce5d9178b
devkarx <aradhydeokar@gmail.com>
2025-07-04 17:36:18 +0530

    Changed theme

--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -1,53 +1,61 @@
 import React from 'react'
-import {Logo,LogoutBtn, Container} from "../index"
-import { Link, useNavigate } from 'react-router-dom'
-import { useSelector } from 'react-redux'
-import authService from '../../appwrite/auth'
+import {Container, Logo, LogoutBtn} from '../index'
+import { Link } from 'react-router-dom'
+import {useSelector} from 'react-redux'
+import { useNavigate } from 'react-router-dom'
 
 function Header() {
+  const authStatus = useSelector((state) => state.auth.status)
   const navigate = useNavigate()
-  const authStatus= useSelector((state)=>{state.auth.status} )
-  const navItems=[
+
+  const navItems = [
     {
-      name: "Home",
-      slug:"/",
-      active: true,
-    },
+      name: 'Home',
+      slug: "/",
+      active: true
+    }, 
     {
       name: "Login",
-      slug:"/login",
+      slug: "/login",
       active: !authStatus,
-    },
-    {
+  },
+  {
       name: "Signup",
-      slug:"/signup",
+      slug: "/signup",
       active: !authStatus,
-    },
-    {
+  },
+  {
       name: "All Posts",
-      slug:"/all-posts",
+      slug: "/all-posts",
       active: authStatus,
-    },
-    {
+  },
+  {
       name: "Add Post",
-      slug:"/add-post",
+      slug: "/add-post",
       active: authStatus,
-    }
+  },
   ]
+
+
   return (
-    <header>
+    <header className='py-3 shadow bg-gray-800 border-b border-gray-700'>
       <Container>
-        <nav>
-          <div>
-          <Link to="/"><Logo /></Link>
+        <nav className='flex'>
+          <div className='mr-4'>
+            <Link to='/'>
+              <Logo width='280' />
+              </Link>
           </div>
-          <ul>
-            {navItems.map((item)=>
-              item.active ? (
-                <li key={item.name}>
-                  <button onClick={()=>{item.slug}}>{item.name}</button>
-                </li>
-              ): null
+          <ul className='flex ml-auto'>
+            {navItems.map((item) => 
+            item.active ? (
+              <li key={item.name}>
+                <button
+                onClick={() => navigate(item.slug)}
+                className='inline-block px-6 py-2 duration-200 hover:bg-gray-700 text-gray-300 hover:text-white rounded-full'
+                >{item.name}</button>
+              </li>
+            ) : null
             )}
             {authStatus && (
               <li>
@@ -56,7 +64,7 @@ function Header() {
             )}
           </ul>
         </nav>
-      </Container>
+        </Container>
     </header>
   )
 }
